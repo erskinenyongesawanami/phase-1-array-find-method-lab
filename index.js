2df1840f0fe2d9c78f3fd9ad700123436b23947f
erskinenyongesawanami <erskineviper@gmail.com>
2025-06-23 03:02:11 +0300

    add changes made

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(record) {
+  const winRecord = record.find(game => game.result === "W");
+  return winRecord ? winRecord.year : undefined;
+}
+
+// Export the function so it's available to the test file
+module.exports = superbowlWin;
